15f9282bb35b915e4056cab8ccb6f5cdfeea0ce0
Ghorbali Issam <issam.ghorbali@gmail.com>
2021-11-01 15:59:27 +0100

    convert money

--- test.php
+++ test.php
@@ -1,2 +1,11 @@
 <?php
-echo 'hellfffoffsf';
+require_once 'vendor/autoload.php';
+
+use TNkemdilim\MoneyToWords\Converter;
+
+use TNkemdilim\MoneyToWords\Languages as Language;
+
+$converter = new Converter("D", "M", Language::FRENCH);
+
+echo ($converter->convert(23.45));
+echo ($converter->convert("1250000"));
